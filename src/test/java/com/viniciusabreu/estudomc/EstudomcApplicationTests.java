dfdc2981a83b0d6c2318b52a9da6780834f11840
Vinicius de Abreu <vini_ciusbraga@hotmail.com>
2019-07-15 10:30:15 -0300

    Projeto criado

---
+++ src/test/java/com/viniciusabreu/estudomc/EstudomcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.viniciusabreu.estudomc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EstudomcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
