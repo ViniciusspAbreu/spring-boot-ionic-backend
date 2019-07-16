862cbf7288a36a1666d377f6c89f0c3e9784fc5a
Vinicius de Abreu <vini_ciusbraga@hotmail.com>
2019-07-16 10:48:46 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/viniciusabreu/estudomc/services/CategoriaService.java
+++ src/main/java/com/viniciusabreu/estudomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.viniciusabreu.estudomc.domain.Categoria;
 import com.viniciusabreu.estudomc.repositories.CategoriaRepository;
+import com.viniciusabreu.estudomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
