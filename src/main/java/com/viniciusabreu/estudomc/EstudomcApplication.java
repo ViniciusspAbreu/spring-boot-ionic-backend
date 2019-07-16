a75384455029176728b997312de56d9fab8542a1
Vinicius de Abreu <vini_ciusbraga@hotmail.com>
2019-07-16 16:55:47 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/viniciusabreu/estudomc/EstudomcApplication.java
+++ src/main/java/com/viniciusabreu/estudomc/EstudomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.viniciusabreu.estudomc.domain.Categoria;
 import com.viniciusabreu.estudomc.domain.Cidade;
+import com.viniciusabreu.estudomc.domain.Cliente;
+import com.viniciusabreu.estudomc.domain.Endereco;
 import com.viniciusabreu.estudomc.domain.Estado;
 import com.viniciusabreu.estudomc.domain.Produto;
+import com.viniciusabreu.estudomc.domain.enums.TipoCliente;
 import com.viniciusabreu.estudomc.repositories.CategoriaRepository;
 import com.viniciusabreu.estudomc.repositories.CidadeRepository;
+import com.viniciusabreu.estudomc.repositories.ClienteRepository;
+import com.viniciusabreu.estudomc.repositories.EnderecoRepository;
 import com.viniciusabreu.estudomc.repositories.EstadoRepository;
 import com.viniciusabreu.estudomc.repositories.ProdutoRepository;
 
@@ -22,11 +27,15 @@ public class EstudomcApplication implements CommandLineRunner{
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	@Autowired
-	private ProdutoRepository ProdutoRepository;
+	private ProdutoRepository produtoRepository;
 	@Autowired
-	private EstadoRepository EstadoRepository;
+	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudomcApplication.class, args);
@@ -50,7 +59,7 @@ public class EstudomcApplication implements CommandLineRunner{
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 			
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		ProdutoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
@@ -62,11 +71,21 @@ public class EstudomcApplication implements CommandLineRunner{
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2,c3));
 		
-		EstadoRepository.saveAll(Arrays.asList(est1, est2));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA); 
 		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 	
 	
