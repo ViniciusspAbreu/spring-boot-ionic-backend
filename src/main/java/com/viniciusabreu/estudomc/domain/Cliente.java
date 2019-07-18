fefa974fa0d597b8faae31721082032a2200cd14
Vinicius de Abreu <vini_ciusbraga@hotmail.com>
2019-07-18 14:14:02 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/viniciusabreu/estudomc/domain/Cliente.java
+++ src/main/java/com/viniciusabreu/estudomc/domain/Cliente.java
@@ -49,7 +49,7 @@ public class Cliente implements Serializable{
 		this.nome = nome;
 		this.email = email;
 		this.cpfOurCnpj = cpfOurCnpj;
-		this.tipo = tipo.getCod();
+		this.tipo = (tipo== null) ? null : tipo.getCod();
 	}
 
 	public Integer getId() {
